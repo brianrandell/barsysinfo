dca6e1cede01f958bf9f0bf998aa987fe87ece56
Brian A. Randell <brianr@mcwtech.com>
2020-12-01 09:36:21 -0800

    Add initial files for solution

---
+++ src/barsysinfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace barsysinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
