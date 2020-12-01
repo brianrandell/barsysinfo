3a367a9ff7008e4ab6a0053f1cf261fe9f43c819
Brian A. Randell <brianr@mcwtech.com>
2020-12-01 09:46:19 -0800

    Add more detailed Windows information.
    #1

--- src/barsysinfo/Program.cs
+++ src/barsysinfo/Program.cs
@@ -25,9 +25,54 @@ namespace barsysinfo
             Console.WriteLine($"FrameworkName: {frameworkName}");
             Console.WriteLine($"Is64BitOperatingSystem: {Environment.Is64BitOperatingSystem}");
 
+            bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+            OutputWindowsSpecifics(IsWindows);
+
             Console.WriteLine(" ");
             Console.WriteLine("Press any key to exit program.");
             Console.ReadLine();
+        }
+
+        private static void OutputWindowsSpecifics(bool isWindows)
+        {
+            if (isWindows)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("Getting Windows specific system information ...");
+                Console.WriteLine(" ");
+
+                var process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "SystemInfo",
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+                var output = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+
+                string currentLine;
+                using (StringReader sr = new StringReader(output))
+                {
+                    while (sr.Peek() > -1)
+                    {
+                        currentLine = sr.ReadLine();
+                        if (!currentLine.StartsWith("Hotfix(s):"))
+                        {
+                            Console.WriteLine(currentLine);
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
         }
     }
 }
