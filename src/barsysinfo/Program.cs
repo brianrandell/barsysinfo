68e3de82ef2ec27349e69b7f644e69ad5fac7e48
Brian A. Randell <brianr@mcwtech.com>
2020-12-01 09:40:05 -0800

    Update with basic OS agnostic information.

--- src/barsysinfo/Program.cs
+++ src/barsysinfo/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Diagnostics;
+using System.IO;
 
 namespace barsysinfo
 {
@@ -6,7 +11,23 @@ namespace barsysinfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Getting system information ...");
+            Console.WriteLine(" ");
+
+            var frameworkName = Assembly
+                .GetEntryAssembly()?
+                .GetCustomAttribute<TargetFrameworkAttribute>()?
+                .FrameworkName;
+
+            Console.WriteLine($"MachineName: {Environment.MachineName}");
+            Console.WriteLine($"OSVersion: {Environment.OSVersion}");
+            Console.WriteLine($"Version: {Environment.Version}");
+            Console.WriteLine($"FrameworkName: {frameworkName}");
+            Console.WriteLine($"Is64BitOperatingSystem: {Environment.Is64BitOperatingSystem}");
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Press any key to exit program.");
+            Console.ReadLine();
         }
     }
 }
